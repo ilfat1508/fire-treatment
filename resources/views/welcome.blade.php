9a630f0d076a7337104adafa656a6d1c0d73cdd7
Ганиев Ильфат Раисович <IGaniev@nefis.local>
2025-12-28 22:03:39 +0300

    Верстка мобильной версии

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,10 +40,10 @@
         </ul>
 
         <div class="menu-contacts">
-            <a href="tel:+78435232745" class="menu-btn">
+            <a href="tel:+7872696383" class="menu-btn">
                 📞 Позвонить
             </a>
-            <a href="https://wa.me/79656171753" class="menu-btn whatsapp">
+            <a href="https://wa.me/7872696383" class="menu-btn whatsapp">
                 💬 WhatsApp
             </a>
         </div>
@@ -61,48 +61,49 @@
         </p>
         <p class=""></p>
         <p>
-            <a href="tel:+7 (843) 523-27-45" class="head__contact__title">
+            <a href="tel:+7 (987) 269-63-83" class="head__contact__title">
                 <i class="bi bi-telephone-forward-fill"></i>
-                +7 (843) 523-27-45
+                +7 (987) 269-63-83
             </a>
         </p>
         <div class="whatsapp-block-wrapper">
-            <a href="https://wa.clck.bar/79656171753">
+            <a href="https://wa.clck.bar/7872696383">
                 <button class="whatsapp-btn">Заказать звонок
                     <div class="dot"></div>
                 </button>
             </a>
         </div>
         <div class="messengers">
-                <a href="https://telegram.me/LENAR">
-                    <i class="bi bi-telegram"></i>
-                </a>
+            <a href="https://telegram.me/LENAR">
+                <i class="bi bi-telegram"></i>
+            </a>
 
-                <a href="https://instagram.com/lenar">
-                    <i class="bi bi-instagram"></i>
-                </a>
+            <a href="https://instagram.com/lenar">
+                <i class="bi bi-instagram"></i>
+            </a>
 
-                <a href="https://api.whatsapp.com/send?phone=79375959624">
-                    <i class="bi bi-whatsapp"></i>
-                </a>
-                <a class="max-app" href="https://api.whatsapp.com/send?phone=79375959624">
-                    <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                         viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
+            <a href="https://api.whatsapp.com/send?phone=7872696383">
+                <i class="bi bi-whatsapp"></i>
+            </a>
+            {{--            <a class="max-app" href="https://api.whatsapp.com/send?phone=79375959624">--}}
+            {{--                <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg"--}}
+            {{--                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"--}}
+            {{--                     viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">--}}
 
-                        <path fill-rule="evenodd" clip-rule="evenodd" fill="#f16f28" d="M508.2113037,878.3283691
-                            c-75.0068665,0-109.8640442-10.9498291-170.4535217-54.7494507
-                            c-38.3246765,49.2745361-159.6860962,87.7817383-164.9785461,21.8997803c0-49.4570312-10.9499207-91.2491455-23.3598175-136.8737793
-                            c-14.7823486-56.2095337-31.572197-118.8065186-31.572197-209.5082092C117.8472214,282.4710693,295.600647,119.5,506.2038574,119.5
-                            c210.7856445,0,375.9467163,171.0010071,375.9467163,381.6041565
-                            C882.8580322,708.4505005,715.5558472,877.2223511,508.2113037,878.3283691z M511.3137817,306.7434082
-                            c-102.5641174-5.29245-182.4984131,65.6994324-200.2007751,177.023468
-                            c-14.599884,92.1616516,11.3149109,204.3982239,33.3972168,210.2381897
-                            c10.5848999,2.5549316,37.2296753-18.9799194,53.8370361-35.5872192
-                            c27.4610901,18.9708252,59.43927,30.364624,92.7091675,33.0322266
-                            c106.2732849,5.1118774,197.0797424-75.7943115,204.2157288-181.9509583
-                            c4.1541138-106.3812256-77.6702881-196.4854431-183.9584351-202.5732117L511.3137817,306.7434082z"/>
-                    </svg>
-                </a>
+            {{--                        <path fill-rule="evenodd" clip-rule="evenodd" fill="#f16f28" d="M508.2113037,878.3283691--}}
+            {{--                            c-75.0068665,0-109.8640442-10.9498291-170.4535217-54.7494507--}}
+            {{--                            c-38.3246765,49.2745361-159.6860962,87.7817383-164.9785461,21.8997803c0-49.4570312-10.9499207-91.2491455-23.3598175-136.8737793--}}
+            {{--                            c-14.7823486-56.2095337-31.572197-118.8065186-31.572197-209.5082092C117.8472214,282.4710693,295.600647,119.5,506.2038574,119.5--}}
+            {{--                            c210.7856445,0,375.9467163,171.0010071,375.9467163,381.6041565--}}
+            {{--                            C882.8580322,708.4505005,715.5558472,877.2223511,508.2113037,878.3283691z M511.3137817,306.7434082--}}
+            {{--                            c-102.5641174-5.29245-182.4984131,65.6994324-200.2007751,177.023468--}}
+            {{--                            c-14.599884,92.1616516,11.3149109,204.3982239,33.3972168,210.2381897--}}
+            {{--                            c10.5848999,2.5549316,37.2296753-18.9799194,53.8370361-35.5872192--}}
+            {{--                            c27.4610901,18.9708252,59.43927,30.364624,92.7091675,33.0322266--}}
+            {{--                            c106.2732849,5.1118774,197.0797424-75.7943115,204.2157288-181.9509583--}}
+            {{--                            c4.1541138-106.3812256-77.6702881-196.4854431-183.9584351-202.5732117L511.3137817,306.7434082z"/>--}}
+            {{--                    </svg>--}}
+            {{--            </a>--}}
 
         </div>
     </div>
@@ -114,11 +115,11 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon3.png') }}')"></div>
             <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon4.png') }}')"></div>
-{{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon5.jpeg') }}')"></div>--}}
-{{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon6.jpeg') }}')"></div>--}}
-{{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon.png') }}')"></div>--}}
-{{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon2.png') }}')"></div>--}}
-{{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/main.jpeg') }}')"></div>--}}
+            {{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon5.jpeg') }}')"></div>--}}
+            {{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon6.jpeg') }}')"></div>--}}
+            {{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon.png') }}')"></div>--}}
+            {{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/fon2.png') }}')"></div>--}}
+            {{--            <div class="swiper-slide" style="background-image:url('{{ asset('images/hero/main.jpeg') }}')"></div>--}}
         </div>
     </div>
 
@@ -138,10 +139,39 @@
     <div class="blue-block">
         <img src="{{asset('images/mobile/blue-block.svg')}}" alt="">
         <div class="buttons">
-            <button class="whatsapp-btn"><i class="bi bi-fire">ОГНЕЗАЩИТА</i><div class="arrow"></div></button>
-            <button class="whatsapp-btn"><i class="bi bi-shield-shaded">АНТИКОРРОЗИЯ</i><div class="arrow"></div></button>
+            <button class="whatsapp-btn"><i class="bi bi-fire">ОГНЕЗАЩИТА</i>
+                <div class="arrow"></div>
+            </button>
+            <button class="whatsapp-btn"><i class="bi bi-shield-shaded">АНТИКОРРОЗИЯ</i>
+                <div class="arrow"></div>
+            </button>
         </div>
         <button class="whatsapp-btn">Узнать больше</button>
+    </div>
+    <div class="services-containter-title">
+        <hr>
+        <p>НАШИ УСЛУГИ</p>
+        <hr class="second">
+    </div>
+    <div class="services-container">
+        <div class="service">
+            <img src="{{asset('images/mobile/mobile-fire.png')}}" alt="">
+            <p>Огнезащита <br> Конструкций</p>
+        </div>
+        <div class="service">
+            <img src="{{asset('images/mobile/mobile-cor.png')}}" alt="">
+            <p>Антикоррозия <br> Конструкций</p>
+        </div>
+        <div class="service">
+            <img src="{{asset('images/mobile/mobile-fire-treatment.png')}}" alt="">
+            <p>Пожарная <br> Безопасность</p>
+        </div>
+    </div>
+    <div class="services-buttons">
+        <button class="whatsapp-btn">
+            ЗАКАЗАТЬ КОНСУЛЬТАЦИЮ
+            <div class="arrow"></div>
+        </button>
     </div>
 </section>
 </body>
